04fee9c1d40e43c95d0e80a6b4cda0914c900edb
Dmytro Herasymenko <dimger89@gmail.com>
2017-05-23 17:42:04 +0300

    started

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log(3254);
+const express = require('express');
+const app = express();
+
+const port = 3000;
+const contactlist = [
+	{ name: 'Vasa Pupkin', checked: true },
+	{ name: 'Syava Pupkin', checked: true },
+	{ name: 'Vanya Pupkin', checked: false },
+	{ name: 'Petya Pupkin', checked: true },
+];
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port);
+
+app.get('/api/contactlist', (req, res) => {
+	res.json(contactlist);
+});
+
+console.log(`Server is running on port ${port}`);
