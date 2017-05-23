6830e1bdd5e24e82dcbc2e1e18158115e264a62c
Dmytro Herasymenko <dimger89@gmail.com>
2017-05-23 18:51:02 +0300

    mongoose

--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 const express = require('express');
 const app = express();
-
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/play');
+mongoose.Promise = require('bluebird');
+const db = mongoose.connection;
 const port = 3000;
-const contactlist = [
-	{ name: 'Vasa Pupkin', checked: true },
-	{ name: 'Syava Pupkin', checked: true },
-	{ name: 'Vanya Pupkin', checked: false },
-	{ name: 'Petya Pupkin', checked: true },
-];
+
+let userSchema = mongoose.Schema({
+    name: String,
+    checked: Boolean
+});
+
+let User = mongoose.model('User', userSchema);
+
+var anotherUser = new User({ name: 'Zhora', checked: true });
+console.log(anotherUser.name);
+
+anotherUser.save((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('added new user!');
+    User.find(function (err, users) {
+	  if (err) return console.error(err);
+	  console.log(users);
+	});
+  }
+});
+
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('we\'re connected!');
+});
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port);
 
 app.get('/api/contactlist', (req, res) => {
-	res.json(contactlist);
+	User.find(function (err, users) {
+	  if (err) return console.error(err);
+	  res.json(users);
+	});
+	
 });
 
 console.log(`Server is running on port ${port}`);
